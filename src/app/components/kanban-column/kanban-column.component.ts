1b2265f875f45d0a569384b1927890d1ee505f08
XBinhs17 <nguyenbinh17062003@gmail.com>
2025-04-25 16:50:50 +0700

    fix dynamic component for tab in appcard

--- src/app/components/kanban-column/kanban-column.component.ts
+++ src/app/components/kanban-column/kanban-column.component.ts
@@ -1,22 +1,25 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { CdkDropList, CdkDropListGroup, CdkDragDrop, transferArrayItem, moveItemInArray } from '@angular/cdk/drag-drop';
-import { NgFor, NgClass } from '@angular/common';
+import {
+  Component,
+  Input,
+  TemplateRef,
+  signal,
+  input
+} from '@angular/core';
+import { CdkDropList, CdkDragDrop } from '@angular/cdk/drag-drop';
+import { NgClass, NgFor, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'kanban-column',
   standalone: true,
-  imports: [CdkDropList, CdkDropListGroup, NgFor, NgClass],
   templateUrl: './kanban-column.component.html',
-  styleUrls: ['./kanban-column.component.scss']
+  styleUrls: ['./kanban-column.component.scss'],
+  imports: [NgClass, NgFor, CdkDropList, NgTemplateOutlet],
 })
 export class KanbanColumnComponent {
-  @Input() status!: string;
-  @Input() items: any[] = [];
-  @Input() connectedDropLists: string[] = [];
-  @Input() getStatusClass!: (status: string) => string;
-  @Output() itemDropped = new EventEmitter<CdkDragDrop<any[]>>();
-
-  onDrop(event: CdkDragDrop<any[]>) {
-    this.itemDropped.emit(event);
-  }
+  status = input<string>('');
+  items = input<any[]>([]);
+  connectedDropLists = input<string[]>([]);
+  getStatusClass = input<(status: string) => string>(() => '');
+  dropFn = input<(event: CdkDragDrop<any[]>) => void>(() => {});
+  itemTemplate = input<TemplateRef<any> | null>(null);
 }
